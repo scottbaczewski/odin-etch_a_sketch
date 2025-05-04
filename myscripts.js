89664d924ed39a4a842eaf09272f812305be1395
scottbaczewski <130002237+scottbaczewski@users.noreply.github.com>
2025-05-04 11:22:44 -0400

    Create project; Still need funct run after click Didn't commit as often as I should have, but almost done

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('div.container');
+const startButton = document.querySelector('button.start');
+startButton.addEventListener("click", createSquares());
+
+function createSquares() {
+    let numSquares = prompt("How many rows/columns would you like? (Max: 100)");
+    for (let i = 0; i < numSquares * numSquares; i++) {
+        let square = document.createElement('div');
+        square.setAttribute("class", "squareBlue");
+        square.setAttribute("style", `height:${(1 / numSquares) * 100}%`);
+        square.setAttribute("style", `width:${(1 / numSquares) * 100}%`);
+        square.addEventListener("mouseover", () => {
+            square.className = 'squareRed';
+        })
+    container.appendChild(square);
+    }
+}
