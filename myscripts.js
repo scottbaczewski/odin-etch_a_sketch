315e0dfde1579b98e15a1af6f84ea2bb0617aa6a
scottbaczewski <130002237+scottbaczewski@users.noreply.github.com>
2025-05-04 12:04:04 -0400

    Finish main project; Still have extra credit

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,24 @@
 const container = document.querySelector('div.container');
 const startButton = document.querySelector('button.start');
-startButton.addEventListener("click", createSquares);
+startButton.addEventListener("click", start);
 
-function createSquares() {
+function start() {
     container.innerHTML = '';
-    let numSquares = prompt("How many rows/columns would you like? (Max: 100)");
-    for (let i = 0; i < numSquares * numSquares; i++) {
+    const numSquares = prompt("How many rows/columns would you like? (Max: 100)");
+    if (numSquares > 0 && numSquares <= 100) {
+        createSquares(numSquares);
+    } else {
+        alert("Please enter a number between 1 and 100");
+        return start();
+    }
+}
+
+function createSquares(num) {
+    for (let i = 0; i < num * num; i++) {
         let square = document.createElement('div');
         square.setAttribute("class", "squareBlue");
-        square.setAttribute("style", `height:${(1 / numSquares) * 100}%`);
-        square.setAttribute("style", `width:${(1 / numSquares) * 100}%`);
+        square.setAttribute("style", `height:${(1 / num) * 100}%`);
+        square.setAttribute("style", `width:${(1 / num) * 100}%`);
         square.addEventListener("mouseover", () => {
             square.className = 'squareRed';
         })
